e0e153e2c837381c2fad067b71fae469105a881a
Artem Bushmelev <bushmelevfront@gmail.com>
2024-05-13 20:45:48 +0300

    02_02_03fix1

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -53,7 +53,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
 
   const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
     // need to fix any
-    setName(e.currentTarget.value); // need to fix
+    setName(e.currentTarget.value.trim()); // need to fix
 
     error && setError("");
   };
