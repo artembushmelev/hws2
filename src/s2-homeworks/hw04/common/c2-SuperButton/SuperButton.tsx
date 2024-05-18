88d875f5825c5ad44ef247838e122c3bfa761c64
Artem Bushmelev <bushmelevfront@gmail.com>
2024-05-18 19:37:06 +0300

    commit_02_04_fix3

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,9 +17,15 @@ const SuperButton: React.FC<SuperButtonPropsType> = ({
   disabled,
   ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
 }) => {
-  const finalClassName = `${s.button}${
-    disabled ? " " + s.disabled : xType === "red" ? " " + s.red : ""
-  }${xType === "secondary" ? " " + s.secondary : ""}`;
+  const finalClassName = `${s.button} ${
+    disabled
+      ? s.disabled
+      : xType === "red"
+      ? s.red
+      : xType === "secondary"
+      ? s.secondary
+      : s.default
+  }`;
 
   return (
     <button
